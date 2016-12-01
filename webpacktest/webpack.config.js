ff32c0e24807b2e6b0ed54dd9f589b1f4d9878db
mark-TuWen <1501839339@qq.com>
2016-12-01 15:04:01 +0800

    webpack

--- webpacktest/webpack.config.js
+++ webpacktest/webpack.config.js
@@ -1,14 +1,17 @@
 var webpack = require('webpack')
 
 module.exports = {
-  entry: 'entry.js',
+  entry: './index.js',
   output: {
     path: __dirname,
-    filename: 'bundle.js'
+    filename: './bundle.js'
   },
   module: {
     loaders: [
       {test: /\.css$/, loader: 'style!css'}
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('This file is test webpack')
+  ]
 }
